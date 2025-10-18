4a01e09f28d0f93c14491040f5ae5c3d14c0e8f2
beykyle <beykyle@umich.edu>
2025-10-18 11:07:40 -0400

    fix imports in pro

--- bfrescoxpro_pypkg/setup.py
+++ bfrescoxpro_pypkg/setup.py
@@ -52,7 +52,7 @@ for flag, default in FRESCOX_FLAG_DEFAULTS:
 
 # Package metadata
 PYTHON_REQUIRES = ">=3.9"
-CODE_REQUIRES = []
+CODE_REQUIRES = ["numpy>=1.21", "pandas>=1.3"]
 TEST_REQUIRES = []
 INSTALL_REQUIRES = CODE_REQUIRES + TEST_REQUIRES
 
