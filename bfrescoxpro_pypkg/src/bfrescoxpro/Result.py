e586e2cfbce666281466d64c418c29edd7f47095
beykyle <beykyle@umich.edu>
2025-10-23 15:14:48 -0400

    update bfrescox_pro pypkg given new organization of parsing functionality

--- bfrescoxpro_pypkg/src/bfrescoxpro/Result.py
+++
@@ -1,1 +0,0 @@
-../../../common/Result.py
--- bfrescoxpro_pypkg/src/bfrescoxpro/__init__.py
+++ bfrescoxpro_pypkg/src/bfrescoxpro/__init__.py
@@ -8,31 +8,22 @@ the internal binary as well as to access results.
 from importlib.metadata import version
 
 from ._run_frescox_simulation import (
+    FRESCOX_COREX_SUPPORT,
     FRESCOX_EXE,
-    FRESCOX_MPI_SUPPORT, FRESCOX_OPENMP_SUPPORT,
     FRESCOX_LAPACK_SUPPORT,
-    FRESCOX_COREX_SUPPORT
+    FRESCOX_MPI_SUPPORT,
+    FRESCOX_OPENMP_SUPPORT,
 )
-
+from .Configuration import Configuration
+from .generate_elastic_template import generate_elastic_template
+from .generate_inelastic_template import generate_inelastic_template
 from .information import information
+from .load_tests import load_tests
+from .parallelization_setup import parallelization_setup
+from .parse_differential_xs import absolute_mb_per_sr, ratio_to_rutherford
+from .performance_results import performance_results
 from .print_information import print_information
 from .run_simulation import run_simulation
-
-from .Configuration import Configuration
-from .Result import (
-    read_fort16,
-    read_results_lines,
-    parallelization_setup,
-    performance_results,
-    cross_sections_mbsr,
-    cross_sections_R2R,
-)
-from .generate_inelastic_template import generate_inelastic_template
-from .generate_elastic_template import generate_elastic_template
-
-# ----- Python unittest-based test framework
-# Used for automatic test discovery
-from .load_tests import load_tests
 
 # Allow users to run full test suite as bfrescox.test()
 from .test import test
