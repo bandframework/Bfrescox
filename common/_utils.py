6f56f8473212f7bdd70112865278309f76b11470
beykyle <beykyle@umich.edu>
2025-10-27 19:19:49 -0400

    handle non-fraction inputs

--- common/_utils.py
+++ common/_utils.py
@@ -1,3 +1,6 @@
+from fractions import Fraction
+
+
 def _is_fraction_integer_or_half_integer(f):
     """
     Args:
@@ -7,4 +10,6 @@ def _is_fraction_integer_or_half_integer(f):
       True if the fraction represents an even integer or
         half-integer, False otherwise.
     """
-    return f.denominator in [1, 2]
+    if isinstance(f, Fraction):
+        return f.denominator in [1, 2]
+    return (f * 2).is_integer()
