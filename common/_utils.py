4e30bfcdfcb532721525ee14c30fa5711fd65990
beykyle <beykyle@umich.edu>
2025-10-29 14:28:01 -0400

    allow for ints

--- common/_utils.py
+++ common/_utils.py
@@ -4,7 +4,7 @@ from fractions import Fraction
 def _is_fraction_integer_or_half_integer(f):
     """
     Args:
-      f: A fractions.Fraction object.
+      f: A fractions.Fraction object, int, or float.
 
     Returns:
       True if the fraction represents an even integer or
@@ -12,4 +12,4 @@ def _is_fraction_integer_or_half_integer(f):
     """
     if isinstance(f, Fraction):
         return f.denominator in [1, 2]
-    return (f * 2).is_integer()
+    return float(f * 2).is_integer()
