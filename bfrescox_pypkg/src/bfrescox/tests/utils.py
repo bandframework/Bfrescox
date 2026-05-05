5f7be94482bd1cdddd76849e1771f74c1631bc3d
Jared O'Neal <joneal@anl.gov>
2026-05-05 15:36:44 -0500

    Cleaning tests and utils as part of PR review.

--- bfrescox_pypkg/src/bfrescox/tests/utils.py
+++ bfrescox_pypkg/src/bfrescox/tests/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 def compare_arrays(result, expected, abs_diff_tolr, rel_diff_tolr):
+    assert (abs_diff_tolr >= 0.0) and (rel_diff_tolr >= 0.0)
     if (abs_diff_tolr == 0.0) and (rel_diff_tolr == 0.0):
         np.testing.assert_equal(result, expected)
     else:
