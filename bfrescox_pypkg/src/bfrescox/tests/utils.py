c36a89e0430dfecea02dc9b6311c07413ff318f3
Jared O'Neal <joneal@anl.gov>
2026-05-05 15:58:36 -0500

    Cleaning test as part of PR review.
    Tests passing locally.

--- bfrescox_pypkg/src/bfrescox/tests/utils.py
+++ bfrescox_pypkg/src/bfrescox/tests/utils.py
@@ -6,6 +6,8 @@ def compare_arrays(result, expected, abs_diff_tolr, rel_diff_tolr):
     if (abs_diff_tolr == 0.0) and (rel_diff_tolr == 0.0):
         np.testing.assert_equal(result, expected)
     else:
+        # Assume that if only one tolerance is positive that the user would
+        # never intend for us to check that the arrays are also identical.
         if abs_diff_tolr > 0.0:
             np.testing.assert_allclose(
                 result,
